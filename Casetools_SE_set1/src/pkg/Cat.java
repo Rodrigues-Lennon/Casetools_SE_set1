447feb2c52162a73ffadf33c594c96cd9e71070c
exam <gavinsouza3@gmail.com>
2024-04-29 12:33:16 +0530

    commit

---
+++ Casetools_SE_set1/src/pkg/Cat.java
@@ -0,0 +1,14 @@
+package pkg;
+
+public class Cat {
+
+	private String name;
+	private String colour;
+	public Cat (String name,String breed) {
+		this.name=name;
+		this.colour=colour;
+	}
+	public String getColour() {
+		return colour;
+	}
+}
